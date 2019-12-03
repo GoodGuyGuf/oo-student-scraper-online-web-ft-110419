c89ce0ab72891bc6511e5e704a26087348530917
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-12-03 19:28:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,7 +12,7 @@ class Scraper
 
       {:name => student.css("h4").text,
       :location => student.css("p").text,
-      :profile_url => student.css('href')}
+      :profile_url => student.attr('href')}
 
   end
 end
