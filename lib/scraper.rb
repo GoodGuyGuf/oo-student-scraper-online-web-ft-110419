4c17a72b7f3d80df81cff70cfabca7b296c7eaab
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-12-03 19:34:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -41,6 +41,7 @@ end
       return_hash[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text
       return_hash[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder p").text
       return_hash
+      binding.pry
     end
 
 end
